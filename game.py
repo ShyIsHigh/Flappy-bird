b9a41712fbee788c0ea6479022b2eda33e917370
ShyIsHigh <kold92120@gmail.com>
2026-08-01 12:07:26 -0500

    Add files via upload

--- game.py
+++ game.py
@@ -2,7 +2,19 @@ from asyncio import run_coroutine_threadsafe
 
 from pygame import *
 from random import randint
+import sounddevice as sd
+import numpy as np
 
+sr = 16000
+block = 256
+mic_level = 0.01
+
+def audio_cb(indata, frames, time, status):
+    global mic_level
+    if status:
+        return
+    rms = float(np.sqrt(np.mean(indata**2)))
+    mic_level = .85 * mic_level + 0.15 * rms
 init()
 window_size = 1200, 800
 window = display.set_mode(window_size)
@@ -31,58 +43,84 @@ pipes = generate_pipes(150)
 main_font = font.Font(None, 100)
 score = 0
 lose = False
-y_vel = 2
+y_vel = 0.0
 
-while True:
-    for e in event.get():
-        if e.type == QUIT:
-            quit()
-    window.fill("skyblue")
-    window.blit(bird_texture, player_rect)
+wait = 40
+gravity = 0.6
+THRESH = 0.001
+IMPUSE = -8.0
+
+with sd.InputStream(callback=audio_cb, channels=1, samplerate=sr, blocksize=block):
+    while True:
+        for e in event.get():
+            if e.type == QUIT:
+                quit()
+
+        if mic_level > THRESH:
+            y_vel = IMPUSE
+
+        y_vel += gravity
+        player_rect.y += int(y_vel)
+
+        window.fill("skyblue")
+        window.blit(bird_texture, player_rect)
 
 
-    for pie, is_top in pipes[:]:
-        if not lose:
-            pie.x -= 10
-        pipe_img = transform.scale(pipe_texture, (pie.width, pie.height))
-        if is_top:
-            pipe_img = transform.flip(pipe_img, False, True)
-        window.blit(pipe_img, pie)
+        for pie, is_top in pipes[:]:
+            if not lose:
+                pie.x -= 10
+            pipe_img = transform.scale(pipe_texture, (pie.width, pie.height))
+            if is_top:
+                pipe_img = transform.flip(pipe_img, False, True)
+            window.blit(pipe_img, pie)
 
-        if pie.x <= - pie.width:
-            pipes.remove((pie, is_top))
-            score += 0.5
+            if pie.x <= - pie.width:
+                pipes.remove((pie, is_top))
+                score += 0.5
 
-        if player_rect.colliderect(pie):
-            lose = True
+            if player_rect.colliderect(pie):
+                lose = True
 
-    if len(pipes) < 10:
-        pipes += generate_pipes(10)
+        if len(pipes) < 10:
+            pipes += generate_pipes(10)
 
-    score_text = main_font.render(f'Score: {int(score)}', True, "black")
-    center_text = window_size[0]//2-score_text.get_rect().w//2
-    window.blit(score_text, (center_text, 20))
-    keys = key.get_pressed()
-    if keys[K_w] and not lose:
-        player_rect.y -= 15
-    if keys[K_s] and not lose:
-        player_rect.y += 15
+        score_text = main_font.render(f'Score: {int(score)}', True, "black")
+        center_text = window_size[0]//2-score_text.get_rect().w//2
+        window.blit(score_text, (center_text, 40))
 
-    if keys[K_r] and lose:
-        lose = False
-        score= 0
-        pipes = generate_pipes(150)
-        player_rect.y = window_size[1] // 2 - 100
-        y_vel = 2
+        display.update()
+        clock.tick(60)
 
-    if player_rect.y >= window_size[1] - player_rect.height:
-        lose = True
+        keys = key.get_pressed()
+        if keys[K_w] and not lose:
+            player_rect.y -= 15
+        if keys[K_s] and not lose:
+            player_rect.y += 15
 
-    if lose:
-        player_rect.y += y_vel
-        y_vel *= 1.1
-        if y_vel > 50:
-            y_vel = 50
+        if keys[K_r] and lose:
+            lose = False
+            score= 0
+            pipes = generate_pipes(150)
+            player_rect.y = window_size[1] // 2 - 100
+            y_vel = 0.0
 
-    display.update()
-    clock.tick(60)
+        if player_rect.bottom >= window_size[1]:
+            player_rect.bottom = window_size[1]
+            y_vel = 0.0
+
+        if player_rect.top <= 0:
+            player_rect.top = 0
+            if y_vel < 0:
+                y_vel = 0.0
+
+        if lose and wait > 1:
+            for pie, _ in pipes:
+                pie.x += 8
+            wait -= 1
+        else:
+            lose = False
+            wait = 40
+
+
+        display.update()
+        clock.tick(60)
